2e19173226ff37e2a00a22cc2a801ee6d79cacd4
Farkhad73 <110536132+Farkhad73@users.noreply.github.com>
2022-09-09 10:42:52 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,21 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+     let scores = result.input
+    let bad = 0;
+    let good = 0
+    for (let i = 0; i < qimetler.length; i++) {
+        const element = qimetler[i]
+        if (element == 2) {
+            bad++
+        } if (element == 5) {
+            good++
+        }
+    } if (bad > good) {
+        console.log("i");
+    } if (good > bad) {
+        console.log("i");
+    } else {
+        console.log("i");
   
 });
