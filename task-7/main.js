4c0b36c7e27f0adb92afe96110ee987e4c71519f
Farkhad73 <110536132+Farkhad73@users.noreply.github.com>
2022-09-09 12:11:24 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -4,6 +4,12 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    \let n = result.input;
+    let count = 1;
+    for (let i = 0; i<n.length; i++){
+        if(n[i] == n[i+1]){
+            count++
+        }
+    }console.log(count);
   
 });
